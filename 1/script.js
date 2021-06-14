89fdf612190b4781d30e009fb38d65ecdec7ef7d
KKaciusis <kkaciusis@yahoo.com>
2021-06-14 15:10:43 +0200

    initial commit + first task

---
+++ 1/script.js
@@ -0,0 +1,9 @@
+"use strict"
+document.getElementById('CheckBtn').addEventListener('click', () => {
+
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
+    let max = Math.max(input1, input2);
+
+    document.getElementById('result').setAttribute("placeholder", `${max}`)
+});
