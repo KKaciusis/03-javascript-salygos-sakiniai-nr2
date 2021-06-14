422e66183bf100be83255f2222fa249b42fee6ce
KKaciusis <kkaciusis@yahoo.com>
2021-06-14 15:32:50 +0200

    submit

--- 2/script.js
+++ 2/script.js
@@ -1,10 +1,9 @@
+
 "use strict"
 document.getElementById('CheckBtn').addEventListener('click', () => {
-
-    let input1 = document.getElementById("input1").value;
-    let input2 = document.getElementById("input2").value;
-    let input3 = document.getElementById("input3").value;
-    let max = Math.max(input1, input2, input3);
-
-    document.getElementById('result').setAttribute("placeholder", `${max}`)
+    document.getElementById('result').value = Math.max(
+        document.getElementById("input1").value,
+        document.getElementById("input2").value,
+        document.getElementById("input3").value
+    );;
 });
