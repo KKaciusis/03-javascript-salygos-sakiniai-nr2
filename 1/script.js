ba163d45e507e103b8207074cc10857cd0bd48f0
KKaciusis <kkaciusis@yahoo.com>
2021-06-15 11:00:14 +0200

    hot-fix

--- 1/script.js
+++ 1/script.js
@@ -5,5 +5,5 @@ document.getElementById('CheckBtn').addEventListener('click', () => {
     let input2 = document.getElementById("input2").value;
     let max = Math.max(input1, input2);
 
-    document.getElementById('result').setAttribute("placeholder", `${max}`)
+    document.getElementById('result').value = Math.max;
 });
